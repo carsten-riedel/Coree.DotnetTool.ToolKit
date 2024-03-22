21c26741f9ef96e16ccef6ae005aa00ef568bc8f
Updated form Workflow <carstenriedel@outlook.com>
2024-03-22 08:41:38 +0100

    cleanup

--- src/Projects/Coree.DotnetTool.ToolKit/Command/CommandExistsAsyncCommand.cs
+++ src/Projects/Coree.DotnetTool.ToolKit/Command/CommandExistsAsyncCommand.cs
@@ -2,10 +2,12 @@
 using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
-using Coree.NETStandard.Services.FileService;
+using Coree.NETStandard.Services;
 using Microsoft.Extensions.Logging;
 using Serilog.Core;
 using Serilog.Events;
+
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace Coree.DotnetTool.ToolKit.Command
@@ -27,13 +29,24 @@ namespace Coree.DotnetTool.ToolKit.Command
             [DefaultValue(false)]
             [CommandOption("-t|--throwError")]
             public bool ThrowError { get; init; }
+
+            public override ValidationResult Validate()
+            {
+
+                if (String.IsNullOrWhiteSpace(CommandName))
+                {
+                    return ValidationResult.Error("Required argument <CommandName> cannot be empty");
+                }
+
+                return ValidationResult.Success();
+            }
         }
 
         private readonly ILogger<CommandExistsAsyncCommand> logger;
         private readonly IFileService fileService;
         private readonly LoggingLevelSwitch loggingLevelSwitch;
 
-        public CommandExistsAsyncCommand(ILogger<CommandExistsAsyncCommand> logger, LoggingLevelSwitch loggingLevelSwitch, Coree.NETStandard.Services.FileService.IFileService fileService)
+        public CommandExistsAsyncCommand(ILogger<CommandExistsAsyncCommand> logger, LoggingLevelSwitch loggingLevelSwitch, Coree.NETStandard.Services.IFileService fileService)
         {
             this.logger = logger;
             this.fileService = fileService;
@@ -43,19 +56,20 @@ namespace Coree.DotnetTool.ToolKit.Command
         public override async Task<int> ExecuteAsync(CommandContext context, CommandExistsSettings settings)
         {
             loggingLevelSwitch.MinimumLevel = Serilog.Events.LogEventLevel.Information;
-            return await ExecuteCancelAsync(settings,Program.cancellationTokenSource.Token);
+            return await ExecuteCancelAsync(settings, Program.cancellationTokenSource.Token);
         }
 
-        private async Task<int> ExecuteCancelAsync(CommandExistsSettings settings,CancellationToken cancellationToken)
+        private async Task<int> ExecuteCancelAsync(CommandExistsSettings settings, CancellationToken cancellationToken)
         {
             try
             {
-                var foundat = await fileService.IsCommandAvailableAsync(settings.CommandName);
-                if (settings.ThrowError && foundat ==null)
+                var foundat = await fileService.IsCommandAvailableAsync(settings.CommandName, cancellationToken);
+                if (settings.ThrowError && foundat == null)
                 {
                     logger.LogError("Command {CommandName} not found. Throwing error exitcode.", settings.CommandName);
                     return -99;
-                } else if (foundat == null)
+                }
+                else if (foundat == null)
                 {
                     logger.LogError("Command {CommandName} not found.", settings.CommandName);
                     return 0;
@@ -65,7 +79,6 @@ namespace Coree.DotnetTool.ToolKit.Command
                     logger.LogInformation("Command {CommandName} found in {location}", System.IO.Path.GetFileName(foundat), foundat);
                     return 0;
                 }
-
             }
             catch (OperationCanceledException ex)
             {
@@ -77,8 +90,6 @@ namespace Coree.DotnetTool.ToolKit.Command
                 logger.LogError(ex, "Exception");
                 return -2;
             }
-
-
         }
     }
 }
--- src/Projects/Coree.DotnetTool.ToolKit/Program.cs
+++ src/Projects/Coree.DotnetTool.ToolKit/Program.cs
@@ -7,16 +7,15 @@ using Coree.NETStandard.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Serilog.Core;
-
 using Spectre.Console.Cli;
 
 namespace Coree.DotnetTool.ToolKit
 {
     public class Program
     {
-        public static LoggingLevelSwitch levelSwitch = new LoggingLevelSwitch();
-        public static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-        public static int ExitCode = -1;
+        internal static LoggingLevelSwitch levelSwitch = new LoggingLevelSwitch();
+        internal static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        internal static int ExitCode = -1;
 
         public static async Task<int> Main(string[] args)
         {
@@ -44,11 +43,11 @@ namespace Coree.DotnetTool.ToolKit
                 )
             );
 
-            services.AddSingleton<ILoggingService, ConsoleLoggingService>();
-            services.AddSingleton<Coree.NETStandard.Services.FileService.IFileService, Coree.NETStandard.Services.FileService.FileService>();
+
+            services.AddSingleton<Coree.NETStandard.Services.IFileService, Coree.NETStandard.Services.FileService>();
             services.AddSingleton<LoggingLevelSwitch>(levelSwitch);
 
-            var registrar = new TypeRegistrar(services);
+            var registrar = new Coree.NETStandard.TypeRegistrar(services);
 
             // Create a new command app with the registrar
             // and run it with the provided arguments.
